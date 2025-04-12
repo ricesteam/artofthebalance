1f3e9e0abb5b5b9feafaeaaedf1d911a27c50a15
ricesteam <ricesteam@proton.me>
2025-04-12 13:41:55 -0400

    feat: add main.js entry point in src directory

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { Start } from './scenes/Start.js';
+import { AABB } from './lib/yuka.js';
+
+const config = {
+    type: Phaser.AUTO,
+    title: 'Overlord Rising',
+    description: '',
+    parent: 'game-container',
+    width: 1280,
+    height: 720,
+    backgroundColor: '#000000',
+    pixelArt: false,
+    scene: [Start],
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+};
+
+new Phaser.Game(config);
