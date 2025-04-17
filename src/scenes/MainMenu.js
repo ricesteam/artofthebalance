b010b2151ecbed065fe1b8ff84909f4c34a99126
ricesteam <ricesteam@proton.me>
2025-04-17 11:07:55 -0400

    feat: update main menu scene with new background and title screen

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -1,28 +1,30 @@
 import { Scene } from 'phaser';
 
-export class MainMenu extends Scene
-{
-    constructor ()
-    {
+export class MainMenu extends Scene {
+    constructor() {
         super('MainMenu');
     }
 
-    create ()
-    {
-        this.add.image(512, 384, 'background');
+    create() {
+        const width = this.scale.width;
+        const height = this.scale.height;
 
-        this.add.image(512, 300, 'logo');
+        this.add.image(width / 2, height / 2, 'clouds');
+        this.add.image(width / 2, height / 2, 'titlescreen');
 
-        this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+        // this.add
+        //     .text(width / 2, height / 2 + 100, 'Main Menu', {
+        //         fontFamily: 'Arial Black',
+        //         fontSize: 38,
+        //         color: '#ffffff',
+        //         stroke: '#000000',
+        //         strokeThickness: 8,
+        //         align: 'center',
+        //     })
+        //     .setOrigin(0.5);
 
         this.input.once('pointerdown', () => {
-
             this.scene.start('GameScene');
-
         });
     }
 }
