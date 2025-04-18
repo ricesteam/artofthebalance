8609a25ec521e06e8109a806171d7e336b6bec7e
ricesteam <ricesteam@proton.me>
2025-04-17 20:48:04 -0400

    feat: Implement basic Lawyer enemy with movement and damage logic

--- src/Lawyer.js
+++ src/Lawyer.js
@@ -1,40 +1,80 @@
 import Phaser from 'phaser';
 
+// this class share alot of commonality with the Enemy, refactor it to extend Enemy instead ai!
 export class Lawyer extends Phaser.Physics.Matter.Sprite {
     constructor(scene, x, y) {
         super(scene.matter.world, x, y, 'lawyer', 0, {
             label: 'lawyer',
+            shape: {
+                type: 'rectangle',
+                width: 16,
+                height: 32,
+            },
+            collisionFilter: {
+                category: scene.CATEGORY_ENEMY,
+            },
             density: 0.001,
             friction: 0.01,
             restitution: 0.8,
             frictionAir: 0.005,
-            frictionStatic: 0.0
+            frictionStatic: 0.0,
         });
+        this.scene = scene;
+        this.world = scene.matter.world;
+        this.matter = scene.matter;
 
         scene.add.existing(this);
-
-        this.scene = scene;
+        this.setCollisionGroup(-1);
+        this.setCollidesWith([
+            this.scene.CATEGORY_BLOCK,
+            this.scene.CATEGORY_PLAYER,
+            this.scene.CATEGORY_ATTACK,
+        ]); // Collide with blocks, player, and attack
+        this.setScale(2);
+        this.setRotation(0);
         this.speed = 5;
+        this.isIdle = false; // New state: is the enemy idling?
+        this.idleTimer = null; // Timer for idling
+        this.ignorePlatformRotation = false;
     }
 
     update() {
-        // Basic movement example
-        const cursors = this.scene.input.keyboard.createCursorKeys();
+        if (!this.active) return;
 
-        if (cursors.left.isDown) {
-            this.setVelocityX(-this.speed);
-        } else if (cursors.right.isDown) {
-            this.setVelocityX(this.speed);
-        } else {
-            this.setVelocityX(0);
+        if (!this.ignorePlatformRotation)
+            this.rotation = this.scene.platform.rotation;
+
+        if (this.isIdle) {
+            return; // Do nothing if idling
         }
 
-        if (cursors.up.isDown) {
-            this.setVelocityY(-this.speed);
-        } else if (cursors.down.isDown) {
-            this.setVelocityY(this.speed);
-        } else {
-            this.setVelocityY(0);
+        // Basic back and forth movement
+        if (this.x < this.startPosition - this.range) {
+            this.enemyDirection = 1;
+            this.flipX = false;
+        } else if (this.x > this.startPosition + this.range) {
+            this.enemyDirection = -1;
+            this.flipX = true;
         }
+
+        this.setVelocityX(this.enemyDirection * this.maxSpeed);
+
+        // Randomly start idling
+        if (Phaser.Math.Between(0, 200) === 0) {
+            this.startIdling();
+        }
+    }
+
+    takeDamage(damage) {
+        this.hp -= damage;
+        if (this.hp <= 0) {
+            this.die();
+        }
+    }
+
+    die() {
+        const id = this.scene.enemies.indexOf(this);
+        this.scene.enemies.splice(id, 1);
+        super.destroy();
     }
 }
