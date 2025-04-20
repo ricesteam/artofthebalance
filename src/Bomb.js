b49beb1828053d2997cfb483e48524763c674446
ricesteam <ricesteam@proton.me>
2025-04-20 12:29:54 -0400

    fix: Use correct particle texture for bomb explosion

--- src/Bomb.js
+++ src/Bomb.js
@@ -63,14 +63,14 @@ export class Bomb extends Phaser.Physics.Matter.Sprite {
         });
 
         // Add particle effects
-        this.scene.add.particles(this.x, this.y, 'blood', {
+        this.scene.add.particles(this.x, this.y, 'meatbomb', {
             speed: { min: -200, max: 200 },
             angle: { min: 0, max: 360 },
             scale: { start: 0.5, end: 0 },
             lifespan: 500,
             gravityY: 300,
             quantity: 20,
-            tint: [0xff0000, 0x8b0000], // Red and dark red tints
+            tint: [0xff0000, 0x8b0000], // use brown, yellow, and orange tints instead ai!
             stopAfter: 100, // Stop emitting after 20 particles
         });
     }
