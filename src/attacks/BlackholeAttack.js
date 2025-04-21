154bc76f96e0aa56a73edd086a5718819256720c
ricesteam <ricesteam@proton.me>
2025-04-20 20:37:22 -0400

    refactor: Add count parameter to BlackholeAttack constructor

--- src/attacks/BlackholeAttack.js
+++ src/attacks/BlackholeAttack.js
@@ -2,11 +2,12 @@ import Phaser from 'phaser';
 import { Blackhole } from '../Blackhole';
 
 export class BlackholeAttack {
-    constructor(scene) {
+    constructor(scene, count = 2) {
         this.scene = scene;
         this.name = 'BlackholeAttack';
         this.cooldown = 5000; // Cooldown in milliseconds (e.g., 5 seconds)
         this.lastUsedTime = 0; // Timestamp of the last time the attack was used
+        this.count = count ?? 2;
     }
 
     use(player) {
@@ -17,7 +18,7 @@ export class BlackholeAttack {
 
         this.lastUsedTime = this.scene.time.now;
 
-        // Create a new Blackhole instance at the player's position
+        // create a blackhole for each this.count ai!
         const blackhole = new Blackhole(this.scene, player.x, player.y);
         this.scene.blackholes.push(blackhole); // Add the blackhole to the scene's list
     }
